a5c1fc3d4024db78b8e792098ca4a2f3d820b792
DrdLock <Blackburton23@gmail.com>
2018-11-03 18:44:16 -0700

    Added some messages she can respond to; put important info in a seperate file

---
+++ Ru.rb
@@ -0,0 +1,32 @@
+# Personal Copy with secret:
+# This is a basic setup to get Ru working
+# She can respond when you tell her Welcome Ru and no problem ru
+
+require 'rubygems'
+require 'discordrb'
+require 'json'
+
+file = File.read('db/info.json')
+ru_info = JSON.parse(file)
+
+rubot = Discordrb::Bot.new token: ru_info["token"]
+
+rubot.message(content: "Welcome Ru") do |greeting|
+  sleep 2
+  greeting.respond "Hello #{greeting.user.name}! :)"
+  sleep 3
+  greeting.respond "...unfortunately all I can say right now hello XD."
+end 
+
+rubot.message(content: "Hi Ru") { |hi| hi.respond "Hi #{greeting.user.name}!" }
+
+rubot.message(content: "no problem Ru") do |event|
+  sleep 2
+  event.respond "<3"
+end
+
+rubot.send_message(ru_info["#general"], "Rubot initializing...Hello World")
+rubot.run 
+
+
+
